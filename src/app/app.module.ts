cc6c47d2a49cb959dd99b160974dce840189a5a7
Miguel Shinyenyi <miguelshinyenyi@gmail.com>
2023-02-16 15:42:12 +0300

    save video ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,12 @@ import { MatIconModule } from '@angular/material/icon'
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { SavedVideoDetailsComponent } from './saved-video-details/saved-video-details.component'
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatChipsModule } from '@angular/material/chips';
 
 @NgModule({
   declarations: [
@@ -32,7 +38,13 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     MatButtonModule,
     MatIconModule,
     MatToolbarModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatOptionModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatChipsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/saved-video-details/saved-video-details.component.ts
+++ src/app/saved-video-details/saved-video-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-saved-video-details',
@@ -7,7 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SavedVideoDetailsComponent implements OnInit {
 
-  constructor() { }
+  saveVideoDetailsForm!: FormGroup;
+  title: FormControl = new FormControl('');
+  description: FormControl = new FormControl('');
+  videoStatus: FormControl = new FormControl('')
+
+  constructor() {
+    this.saveVideoDetailsForm = new FormGroup({
+      title: this.title,
+      description: this.description,
+      videoStatus: this.videoStatus
+    });
+  }
 
   ngOnInit(): void {
   }
