4e872c39f5c64ad2d65a7aa433a338f057fc98f1
Miguel Shinyenyi <miguelshinyenyi@gmail.com>
2023-02-21 09:33:59 +0300

    auth0 single signon with Http interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UploadVideoComponent } from './upload-video/upload-video.component';
 import { FormsModule } from '@angular/forms';
 import { NgxFileDropModule } from 'ngx-file-drop';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { MatButtonModule } from '@angular/material/button';
 import { HeaderComponent } from './header/header.component';
 import { MatIconModule } from '@angular/material/icon'
@@ -27,6 +27,8 @@ import { VgBufferingModule } from '@videogular/ngx-videogular/buffering';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { VideoPlayerComponent } from './video-player/video-player.component';
 import { AuthConfigModule } from './auth/auth-config.module';
+import { AuthInterceptor, authInterceptor } from 'angular-auth-oidc-client';
+import { VideoDetailsComponent } from './video-details/video-details.component';
 
 @NgModule({
   declarations: [
@@ -34,7 +36,8 @@ import { AuthConfigModule } from './auth/auth-config.module';
     UploadVideoComponent,
     HeaderComponent,
     SavedVideoDetailsComponent,
-    VideoPlayerComponent
+    VideoPlayerComponent,
+    VideoDetailsComponent
   ],
   imports: [
     BrowserModule,
@@ -60,7 +63,9 @@ import { AuthConfigModule } from './auth/auth-config.module';
     MatSnackBarModule,
     AuthConfigModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
